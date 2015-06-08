4770b8e23d1f276655bec0c9b847b40dcc9dc6ed
shawn234 <shawncpm@gmail.com>
2015-06-07 20:36:08 -0700

    Code Cleanup on Login Button
    Use React state to manage login button state

--- src/js/components/login_bar.js.jsx
+++ src/js/components/login_bar.js.jsx
@@ -1,47 +1,47 @@
 var LoginBar = React.createClass({
-		getInitialState: function() {
-			return {active_user: false};
-		},
+    getInitialState: function() {
+        return {
+            activeUser: false,
+        };
+    },
 
-		componentDidMount: function () {
-				var prodFB_ID = '497341860414071';
-				var devFB_ID = '817044031744170';
-				// Initialize Parse AP ID, JS ID
-  			Parse.initialize("7cV5msyBWxaktixwnKJI6FH9UM88oDCqD4QqnyTo", "qqebJnzSOPhTaSmR8E8L99VAnHFOBzQbMgrYowMY");
+    componentDidMount: function () {
+        var prodFB_ID = '497341860414071';
+        var devFB_ID = '817044031744170';
+        // Initialize Parse AP ID, JS ID
+        Parse.initialize("7cV5msyBWxaktixwnKJI6FH9UM88oDCqD4QqnyTo", "qqebJnzSOPhTaSmR8E8L99VAnHFOBzQbMgrYowMY");
 
-			 window.fbAsyncInit = function() {
-        Parse.FacebookUtils.init({
-          appId      : devFB_ID,
-           status     : true,  // check Facebook Login status
-      		 cookie     : true,  // enable cookies to allow Parse to access the session
-      		 xfbml      : true,  // initialize Facebook social plugins on the page
-      		 version    : 'v2.3' // point to the latest Facebook Graph API version
-        });
+        window.fbAsyncInit = function() {
+            Parse.FacebookUtils.init({
+                appId      : devFB_ID,
+                status     : true,  // check Facebook Login status
+                cookie     : true,  // enable cookies to allow Parse to access the session
+                xfbml      : true,  // initialize Facebook social plugins on the page
+                version    : 'v2.3' // point to the latest Facebook Graph API version
+            });
 
-        FB.getLoginStatus(function(response) {
-	      	this.statusChangeCallback(response);
-	    	}.bind(this));
-      }.bind(this);
+            FB.getLoginStatus(function(response) {
+                this.statusChangeCallback(response);
+            }.bind(this));
+        }.bind(this);
 
-      (function(d, s, id){
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
 		},
 
 		updateButton : function(){
 			FB.api('/me', function(response) {
 					console.log(JSON.stringify(response));
-					document.getElementById('logged-in-msg').innerHTML = 'Sup, ' + response.first_name + ' ';
-					document.getElementById('profile-pic').setAttribute("src", "http://graph.facebook.com/" + response.id + "/picture");
-					document.getElementById('profile-pic').style.display = "inline";
-			    document.getElementById('status').innerHTML = "Logout"
-			});
-			this.state.active_user = true;
+                    this.setState({firstName: response.first_name, responseID: response.id});
+			}.bind(this));
+			this.setState({activeUser: true});
 		},
 
 		statusChangeCallback: function (response) {
@@ -52,11 +52,9 @@ var LoginBar = React.createClass({
 	     	
 	    } else if (response.status === 'not_authorized') {
 	      // The person is logged into Facebook, but not your app.
-	      document.getElementById('status').innerHTML = 'Login to this app';
 	    } else {
 	      // The person is not logged into Facebook, so we're not sure if
 	      // they are logged into this app or not.
-	      document.getElementById('status').innerHTML = 'Login with Facebook';
 	    }
 	  },
 
@@ -67,51 +65,56 @@ var LoginBar = React.createClass({
 	  	},
 
 	  handleClick : function(){
-	  	if(this.state.active_user){
+	  	if(this.state.activeUser){
 	  		console.log('attempt to logout');
 	  		Parse.User.logOut();
-	  		document.getElementById('status').innerHTML = 'Login with Facebook';
-	  		document.getElementById('logged-in-msg').innerHTML = '';
-	  		document.getElementById('profile-pic').style.display = "none";
-	  		document.getElementById('profile-pic').setAttribute("src", "");
-	  		this.state.active_user = false;
+			this.setState({activeUser: false, firstName: '', responseID: ''});
 	  	}
 	  	else {
 	  		console.log('attempt to login');
 	  		var permissions = "public_profile,";
 	  		Parse.FacebookUtils.logIn(permissions, {
-			  success: function(user) {
-			    // Handle successful login
-			      if (!user.existed()) {
-					    alert("User signed up and logged in through Facebook!");
-					  } else {
-					    alert("User logged in through Facebook!");
-					  }
-			    this.updateButton();
-			  }.bind(this),
-			  error: function(user, error) {
-			    // Handle errors and cancellation
-			    document.getElementById('status').innerHTML = 'Login to this app';
-			  }
-			});
-	  	}
-	  },
+                success: function(user) {
+                    // Handle successful login
+			        if (!user.existed()) {
+                        // User signed up and logged in through Facebook
+                    } else {
+                        // User logged in through Facebook
+                    }
+                    this.updateButton();
+                }.bind(this),
+			    error: function(user, error) {
+                    // Handle errors and cancellation
+                    alert("Error logging in");
+			    }
+            });
+        }
+    },
 
     render: function () {
+        var loginGreeting = '';
+        var loginButtonMessage = '';
+        var profilePic = null;
+        if (this.state.activeUser) {
+            loginGreeting = 'Sup, ' + this.state.firstName;
+            loginButtonMessage = 'Logout';
+            profilePic = <img src={'http://graph.facebook.com/' + this.state.responseID + '/picture'} />;
+        } else {
+            loginButtonMessage = 'Login with Facebook';
+        }
         return (
-        		<div>
-	        		<span
-							  className="fb-like"
-							  data-send="true"
-							  data-width="400"
-							  data-show-faces="true">
-							</span>
-							<span id="logged-in-msg"></span>
-							<img id="profile-pic"></img>
-	        		<a id="status" className= "btn btn-primary" href="#" onClick={this.handleClick}>
-	        			Login with Facebook
-		        	</a>
-	        	</div>
+            <div>
+                <span
+                  className="fb-like"
+                  data-send="true"
+                  data-width="400"
+                  data-show-faces="true" />
+                <span>{loginGreeting}</span>
+                {profilePic}
+                <a id="status" className= "btn btn-primary" href="#" onClick={this.handleClick}>
+                    {loginButtonMessage}
+                </a>
+            </div>
         );
     }
 });
