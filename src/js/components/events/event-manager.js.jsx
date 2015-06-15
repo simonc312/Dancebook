ba17f69da1d5ad8cde81524f41754d795377e663
simonc312 <simon.c312@berkeley.edu>
2015-06-15 00:31:55 -0700

    able to view events you have not yet responded to. However need better way to update this.state for EventManager react component. Currently relying on a timeout call

--- src/js/components/events/event-manager.js.jsx
+++ src/js/components/events/event-manager.js.jsx
@@ -2,7 +2,7 @@ var Event = require("./event.js.jsx");
 var EventManager = React.createClass({
     getInitialState: function(){
       return {
-        events:[],
+        events:[], events_not_replied:[]
       };
     },
     fbEnsureInit: function(callback) {
@@ -14,39 +14,62 @@ var EventManager = React.createClass({
             }
         }
     },
-    fetchEvents: function(user_id){
+    fetchEvents: function(user_id,queryString,callback){
       FB.api(
-          "/"+user_id+"/events",
+          "/"+user_id+queryString, // add /not_replied to include those kind of events
           function (response) {
             if (response && !response.error) {
               /* handle the result */
-              this.setState({events: response.data});
-              console.log(response);
+              callback(response.data);
+              //console.log(response);
             }else{
               console.log(response);
             }
           }.bind(this)
       );
     },
-    componentDidMount: function() {
-      /* make the API call */
-      var facebook_user_id = "me"; //replace with Parse.User.facebookUserId later
-      this.fbEnsureInit(function(){this.fetchEvents(facebook_user_id)}.bind(this));;
-    },
-    render: function () {
-        var event_manager_info = <h5>Event Listing Info</h5>
-        var rows = [];
-        if(this.state.events){
-          this.state.events.forEach(function(e){
-            console.log(e);
+    addEventRows: function(eventArray){
+      var rows = [];
+      if(eventArray){
+          eventArray.forEach(function(e){
             rows.push(<Event name={e.name} rsvp={e.rsvp_status} id={e.id} key={e.id} />)
           });
         }
+      return rows;
+    },
+    componentDidMount: function() {
+      /* make the API call */
+      var facebook_user_id = "me"; //replace with Parse.User.facebookUserId later
+      var replied_events_query = "/events";
+      var unreplied_events_query = "/events/not_replied";
+      var events = [];
+      var events_not_replied = [];
+      var eventCallback = function(data){events = data; console.log(events);};
+      var eventNRCallback = function(data){events_not_replied = data; console.log(events_not_replied);};
+      this.fbEnsureInit(function(){
+        this.fetchEvents(facebook_user_id,replied_events_query,eventCallback);
+        this.fetchEvents(facebook_user_id,unreplied_events_query,eventNRCallback);
+      }.bind(this));
+
+      setTimeout(function() {
+        this.setState({
+          events: events, events_not_replied: events_not_replied
+        });
+        console.log('events updated');
+      }.bind(this), 3000); 
+    },
+    render: function () {
+        var event_manager_info = <h5>Event Listing Info</h5>
+        var rows = this.addEventRows(this.state.events);
+        var no_reply_rows = this.addEventRows(this.state.events_not_replied);
         return (
               <div className="container">
                 <div class="row">
                   {event_manager_info}
+                  <h1>Attending Events</h1>
                   {rows}
+                  <h1>Event Invites</h1>
+                  {no_reply_rows}
                 </div>
               </div>
         );
