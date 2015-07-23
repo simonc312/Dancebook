6cbda8bf637c5370b88e65bb4c3ce9e04a181cda
simonc312 <simon.c312@berkeley.edu>
2015-07-22 18:44:42 -0700

    add materialize-css node module and added gulp tasks to watch and copy files. added basic sidebar but its not reading css properly

--- src/js/home.js
+++ src/js/home.js
@@ -15,3 +15,8 @@ React.render(
 
 //initialize skrollr js 
 var s = skrollr.init();
+
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
