a62fe0419e1f2065bd7fdcc97f0f55e6ae5fb304
simonc312 <simon.c312@berkeley.edu>
2016-01-17 17:46:46 -0800

    fix required checkbox position static caused by materialize. debugging why deleteHandler function not being bound to new entries

--- src/js/components/forms/form_field_entry_handlers.js.jsx
+++ src/js/components/forms/form_field_entry_handlers.js.jsx
@@ -6,19 +6,21 @@ var FormFieldHandlers = React.createClass({
       alert("EDIT CLICKED");
     },
     _handleDuplicateClick: function(event){
+      alert("DUPLICATE CLICKED");
     },
     _handleDeleteClick: function(event){
-      alert("FIRE");
+      alert("DELETE CLICKED");
+      this.props.onDeleteHandler();
     },
     render: function () {
-        var handler = this.props.onDeleteHandler;
-        return (
-              <div className="fieldHandler pull-right">
-                <a onClick={this._handleEditClick} title="edit entry"><i className="fa fa-edit fa-3x"></i></a>
-                <a onClick={this._handleDuplicateClick} title="duplicate entry"><i className="fa fa-copy fa-3x"></i></a>
-                <a onClick={this.props.onDeleteHandler} title="delete entry"><i className="fa fa-trash fa-3x"></i></a>
-              </div>
-        );
+      var handler = this.props.onDeleteHandler;
+      return (
+        <div className="fieldHandler pull-right">
+          <a onClick={this._handleEditClick} title="edit entry"><i className="fa fa-edit fa-3x"></i></a>
+          <a onClick={this._handleDuplicateClick} title="duplicate entry"><i className="fa fa-copy fa-3x"></i></a>
+          <a onClick={this._handleDeleteClick} title="delete entry"><i className="fa fa-trash fa-3x"></i></a>
+        </div>
+      );
     }
 });
 window.FormFieldHandlers = FormFieldHandlers;
